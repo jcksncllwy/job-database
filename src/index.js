9a9fb2a36621de5d5f8627f219c5c26436050872
Jackson Callaway <jcksncllwy@gmail.com>
2017-03-20 15:06:07 -0400

    need react-root

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from 'app.js'
 
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('react-root'))
 
